557abc337cb107465a17e60e68f38fd7c10d2f19
Miguel Madero <git@miguelmadero.com>
2016-09-26 14:29:03 -0700

    Fixes this to make sure we support node 0.12 and adds this to travis so we run on it by default

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 
 module.exports = {
     name: 'ember-cli-hot-loader',
-    serverMiddleware: function(config){
+    serverMiddleware: function (config){
         var lsReloader = require('./lib/hot-reloader')(config.options);
         lsReloader.run();
     },
-    included (app) {
+    included: function (app) {
         this._super.included(app);
         // TODO: consider removing this since it adds an unnecessary runtime dependency to all apps
         app.import(app.bowerDirectory + '/ember/ember-template-compiler.js');
