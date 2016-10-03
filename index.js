829bfdfd5787704c774fcf8eec2d11f1a1c403c1
Miguel Madero <git@miguelmadero.com>
2016-10-02 20:47:28 -0700

    Adds a basic transformer to wrap MustacheElements and BlockStatements that we need to wrap

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 /* jshint node: true */
 'use strict';
+var HTMLBarsWrapHelpers = require('./lib/htmlbars/wrap-helpers');
 
 module.exports = {
     name: 'ember-cli-hot-loader',
     serverMiddleware: function (config){
-	// If in production, don't add reloader
-        if (config.options.environment === 'production') {
-            return;
-        }
-        var lsReloader = require('./lib/hot-reloader')(config.options);
-        lsReloader.run();
+      // If in production, don't add reloader
+      if (config.options.environment === 'production') {
+        return;
+      }
+      var lsReloader = require('./lib/hot-reloader')(config.options);
+      lsReloader.run();
+    },
+    setupPreprocessorRegistry: function(type, registry) {
+      registry.add('htmlbars-ast-plugin', {
+        name: "wrap-helpers",
+        plugin: HTMLBarsWrapHelpers
+      });
     }
 };
