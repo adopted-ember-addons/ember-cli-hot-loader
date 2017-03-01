e32f1657beab59ebe2091822358fc0abb1ef4863
Oli Griffiths <oli.griffiths@me.com>
2017-03-01 11:35:23 -0500

    Default `podModulePrefix` to `modulePrefix` if not set
    Remove `appName` from requirejs module resolution
    
    Implement tests
    
    Load clear-requirejs directly
    
    Use sinon for mocks
    
    Remove before/after test hooks

--- addon/utils/clear-requirejs.js
+++ addon/utils/clear-requirejs.js
@@ -2,25 +2,26 @@ import Ember from 'ember';
 
 const { getOwner, get } = Ember;
 
-// Access requirejs global
-const requirejs = window.requirejs;
-
 /**
  * Unsee a requirejs module if it exists
  * @param {String} module The requirejs module name
  */
 function requireUnsee(module) {
-  if (requirejs.has(module)) {
-    requirejs.unsee(module);
+  if (window.requirejs.has(module)) {
+      window.requirejs.unsee(module);
   }
 }
 
-export default function clearContainerCache(context, componentName) {
-  const owner = getOwner(context);
-  const config = owner.resolveRegistration('config:environment');
-  const appName = get(owner, 'base.name') || get(owner, 'application.name');
-  const modulePrefix = get(config, 'modulePrefix') || appName;
-  const podModulePrefix = get(config, 'podModulePrefix');
+/**
+ * Clears the requirejs cache for a component, checking for both "classic"
+ * style components & "pod" style components
+ *
+ * @param {Object} config The applicaiton config
+ * @param {String} componentName The component name being reloaded
+ */
+export function clearRequirejsCache(config, componentName) {
+  const modulePrefix = get(config, 'modulePrefix');
+  const podModulePrefix = get(config, 'podModulePrefix') || modulePrefix;
 
   // Invalidate regular module
   requireUnsee(`${modulePrefix}/components/${componentName}`);
@@ -29,4 +30,17 @@ export default function clearContainerCache(context, componentName) {
   // Invalidate pod modules
   requireUnsee(`${podModulePrefix}/components/${componentName}/component`);
   requireUnsee(`${podModulePrefix}/components/${componentName}/template`);
+}
+
+/**
+ * Clears the requirejs cache for a component, checking for both "classic"
+ * style components & "pod" style components
+ *
+ * @param {Object} component The component that's being reloaded
+ * @param {String} componentName The component name being reloaded
+ */
+export default function (component, componentName) {
+  const owner = getOwner(component);
+  const config = owner.resolveRegistration('config:environment');
+  clearRequirejsCache(config, componentName);
 }
