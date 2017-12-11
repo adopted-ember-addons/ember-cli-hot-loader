4298f0821a0d718cdb7e14dca5501b6328c13727
toran billups <toranb@gmail.com>
2017-12-10 20:56:30 -0600

    EMBER: update the ember-try configuration

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,6 +1,18 @@
 /* eslint-env node */
 module.exports = {
+  useYarn: true,
   scenarios: [
+    {
+      name: 'ember-1.13',
+      bower: {
+        dependencies: {
+          'ember': '~1.13.0'
+        },
+        resolutions: {
+          'ember': '~1.13.0'
+        }
+      }
+    },
     {
       name: 'ember-lts-2.12',
       npm: {
@@ -18,57 +30,11 @@ module.exports = {
       }
     },
     {
-      name: 'ember-release',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#release'
-        },
-        resolutions: {
-          'ember': 'release'
-        }
-      },
+      name: 'ember-lts-2.17',
       npm: {
         devDependencies: {
-          'ember-source': null
+          'ember-source': '~2.17.0'
         }
-      }
-    },
-    {
-      name: 'ember-beta',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#beta'
-        },
-        resolutions: {
-          'ember': 'beta'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': null
-        }
-      }
-    },
-    {
-      name: 'ember-canary',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#canary'
-        },
-        resolutions: {
-          'ember': 'canary'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': null
-        }
-      }
-    },
-    {
-      name: 'ember-default',
-      npm: {
-        devDependencies: {}
       }
     }
   ]
