695d25cc142c8ab908ca81b0f20a503d5bdd28a2
toran billups <toranb@gmail.com>
2017-12-11 20:33:28 -0600

    DEPENDENCY: update to ember-cli v2.17.1

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,17 +3,6 @@ module.exports = {
   useYarn: true,
   scenarios: [
     {
-      name: 'ember-1.13',
-      bower: {
-        dependencies: {
-          'ember': '~1.13.0'
-        },
-        resolutions: {
-          'ember': '~1.13.0'
-        }
-      }
-    },
-    {
       name: 'ember-lts-2.12',
       npm: {
         devDependencies: {
@@ -30,10 +19,26 @@ module.exports = {
       }
     },
     {
-      name: 'ember-lts-2.17',
+      name: 'ember-2.17',
       npm: {
         devDependencies: {
           'ember-source': '~2.17.0'
+        }
+      }
+    },
+    {
+      name: 'ember-beta',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#beta'
+        },
+        resolutions: {
+          'ember': 'beta'
+        }
+      },
+      npm: {
+        devDependencies: {
+          'ember-source': null
         }
       }
     }
