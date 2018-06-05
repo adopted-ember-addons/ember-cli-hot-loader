9dc893135c1045e24f78033887ba4c77ed4d726e
toran billups <toranb@gmail.com>
2018-06-05 06:59:50 -0500

    BUILD: updated beta url for travis

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,38 +1,48 @@
-/* eslint-env node */
-module.exports = {
-  useYarn: true,
-  scenarios: [
-    {
-      name: 'ember-lts-2.12',
-      npm: {
-        devDependencies: {
-          'ember-source': '~2.12.0'
+'use strict';
+
+const getChannelURL = require('ember-source-channel-url');
+
+module.exports = function() {
+  return Promise.all([
+    getChannelURL('release'),
+    getChannelURL('beta')
+  ]).then((urls) => {
+    return {
+      useYarn: true,
+      scenarios: [
+        {
+          name: 'ember-lts-2.12',
+          npm: {
+            devDependencies: {
+              'ember-source': '~2.12.0'
+            }
+          }
+        },
+        {
+          name: 'ember-lts-2.18',
+          npm: {
+            devDependencies: {
+              'ember-source': '~2.18.0'
+            }
+          }
+        },
+        {
+          name: 'ember-3.1',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.1.1'
+            }
+          }
+        },
+        {
+          name: 'ember-beta',
+          npm: {
+            devDependencies: {
+              'ember-source': urls[1]
+            }
+          }
         }
-      }
-    },
-    {
-      name: 'ember-lts-2.18',
-      npm: {
-        devDependencies: {
-          'ember-source': '~2.18.0'
-        }
-      }
-    },
-    {
-      name: 'ember-3.1',
-      npm: {
-        devDependencies: {
-          'ember-source': '~3.1.1'
-        }
-      }
-    },
-    {
-      name: 'ember-3.3',
-      npm: {
-        devDependencies: {
-          'ember-source': '~3.3.0-beta.1'
-        }
-      }
-    }
-  ]
+      ]
+    };
+  });
 };
