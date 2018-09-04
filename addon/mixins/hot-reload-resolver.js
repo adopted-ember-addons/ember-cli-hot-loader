73823b6b9f0853141f0d8810810146968ec0d891
Alex Kanunnikov <lifeart92@gmail.com>
2018-09-04 17:55:33 +0300

    Ability to define "shouldExcludeComponent" method in resolver

--- addon/mixins/hot-reload-resolver.js
+++ addon/mixins/hot-reload-resolver.js
@@ -5,29 +5,45 @@ import { get, computed } from '@ember/object';
 import config from 'ember-get-config';
 import { captureTemplateOptions } from 'ember-cli-hot-loader/utils/clear-container-cache';
 
-function removeOriginalFromParsedName (parsedName) {
-  parsedName.fullName = parsedName.fullName.replace(/-original$/, '');
-  parsedName.fullNameWithoutType = parsedName.fullNameWithoutType.replace(/-original$/, '');
-  parsedName.name= parsedName.name.replace(/-original$/, '');
+const ORIGINAL_COMPONENT_POSTFIX = '-original';
+const TEMPLATE_MATCH_REGEX = new RegExp(/-original$/);
+
+function removeOriginalFromParsedName (parsedName, pattern) {
+  parsedName.fullName = parsedName.fullName.replace(pattern, '');
+  parsedName.fullNameWithoutType = parsedName.fullNameWithoutType.replace(pattern, '');
+  parsedName.name= parsedName.name.replace(pattern, '');
 }
 
-function shouldIgnoreTemplate (parsedName) {
-  return parsedName.fullName.match(/template:components\//) && !parsedName.fullName.match(/\-original$/); // eslint-disable-line
+function shouldIgnoreTemplate (parsedName, pattern) {
+  return parsedName.fullName.match(/template:components\//) && !parsedName.fullName.match(pattern); // eslint-disable-line
 }
 
 export default Mixin.create({
-  resolveOther (parsedName) {
+  originalTemplateMatchRegex: computed(function(){
+    return TEMPLATE_MATCH_REGEX;
+  }),
+  originalComponentPostfix: computed(function(){
+	return ORIGINAL_COMPONENT_POSTFIX;
+  }),
+  shouldExcludeComponent(parsedName) {
+	const excluded = get(this, 'excluded');
+	if (excluded.some((name) => name === parsedName.name)) {
+        return true;
+	} else {
+		return false;
+	}
+  },
+  resolveOther(parsedName) {
     captureTemplateOptions(parsedName);
-
-    if (parsedName.type === 'template' && shouldIgnoreTemplate(parsedName)) {
+    const templateMatchRegex = get(this, 'originalTemplateMatchRegex');
+    if (parsedName.type === 'template' && shouldIgnoreTemplate(parsedName, templateMatchRegex)) {
       return;
     }
 
     const resolved = this._super(...arguments);
     if (parsedName.type === 'component') {
-      const excluded = get(this, 'excluded');
-      if (excluded.some((name) => name === parsedName.name)) {
-        return this._super(parsedName);
+      if (this.shouldExcludeComponent(parsedName)) {
+		return this._super(parsedName);
       }
       if (resolved) {
         return this._resolveComponent(resolved, parsedName);
@@ -35,8 +51,8 @@ export default Mixin.create({
       if (this._resolveOriginalTemplateForComponent(parsedName)) {
         return this._resolveComponent(Component.extend(), parsedName);
       }
-      if (parsedName.fullName.match(/\-original$/)) { // eslint-disable-line
-        removeOriginalFromParsedName(parsedName);
+      if (parsedName.fullName.match(templateMatchRegex)) { // eslint-disable-line
+        removeOriginalFromParsedName(parsedName, templateMatchRegex);
         return this._super(parsedName);
       }
     }
@@ -44,18 +60,19 @@ export default Mixin.create({
     return resolved;
   },
   resolveTemplate (parsedName) {
-    if (shouldIgnoreTemplate(parsedName)) {
+    const templateMatchRegex = get(this, 'originalTemplateMatchRegex');
+    if (shouldIgnoreTemplate(parsedName, templateMatchRegex)) {
       return;
     }
 
-    removeOriginalFromParsedName(parsedName);
+    removeOriginalFromParsedName(parsedName, templateMatchRegex);
     return this._super(...arguments);
   },
   _resolveComponent (resolved, parsedName) {
     return HotReplacementComponent.createClass(resolved, parsedName);
   },
   _resolveOriginalTemplateForComponent (parsedName) {
-    const templateFullName = `template:components/${parsedName.fullNameWithoutType}-original`;
+    const templateFullName = `template:components/${parsedName.fullNameWithoutType}${this.originalComponentPostfix}`;
     const templateParsedName = this.parseName(templateFullName);
     return this.resolveTemplate(templateParsedName) || this.resolveOther(templateParsedName);
   },
